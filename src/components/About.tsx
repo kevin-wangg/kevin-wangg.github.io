58007e4db055436020ca3a12890cd94dcc709e61
Kevin Wang <kevinwang2040@gmail.com>
2022-07-20 22:06:43 -0400

    Test

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -19,7 +19,7 @@ const About = () => {
         <PageContainer>
             <Navbar />
             <SubHeading>
-                Hi there, I'm Kevin!
+                Hi there, I'm poop Kevin!
             </SubHeading>
             <Text>
                 I'm currently a computer science student at the University of Waterloo. I enjoy writing code and building things that can have an impact
